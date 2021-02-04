d8a9ad9de58e43836d4a57b9c5703097e974e63b
hundredvisionsguy <cwinikka@gmail.com>
2021-02-03 17:24:38 -0800

    moving colortools to its own script for future webtool enhancements and adjusting tests accordingly

--- tests/test_webtools.py
+++ tests/test_webtools.py
@@ -5,7 +5,7 @@
 import pytest
 
 
-from webtools import webtools
+import webtools.colortools as color
 
 
 # @pytest.fixture
@@ -24,24 +24,32 @@ from webtools import webtools
 #     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 def test_rgb_to_hex_for_black():
-    results = webtools.rgb_to_hex(0, 0, 0)
+    results = color.rgb_to_hex(0, 0, 0)
     expected = "#000000"
     assert results == expected
 
 
 def test_rgb_to_hex_for_white():
-    results = webtools.rgb_to_hex(255, 255, 255)
+    results = color.rgb_to_hex(255, 255, 255)
     expected = "#ffffff"
     assert results == expected
 
 
 def test_rgb_to_hex_for_336699():
-    results = webtools.rgb_to_hex(51, 102, 153)
+    results = color.rgb_to_hex(51, 102, 153)
     expected = "#336699"
     assert results == expected
 
 
 def test_rgb_to_hex_for_336699():
-    results = webtools.rgb_to_hex("rgb(51,102,153)")
+    results = color.rgb_to_hex("rgb(51,102,153)")
     expected = "#336699"
     assert results == expected
+
+def test_rgb_to_hsl_deepSkyBlue():
+    results = color.rgb_to_hsl((0, 191, 255))
+    expected = (195, 100, 50)
+    assert results == expected
+
+def test_rgb_to_hsl_for_bada55():
+    assert color.rgb_to_hsl((186, 218, 85)) == (74, 64, 59)
