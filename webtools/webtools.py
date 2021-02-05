e2f052369012b05df8b7ba829d29652fa3cdef53
hundredvisionsguy <cwinikka@gmail.com>
2021-02-04 23:18:53 -0800

    adding create_color_palette() function

--- webtools/webtools.py
+++ webtools/webtools.py
@@ -2,16 +2,35 @@
 import math
 import random
 import colortools as color
+import clerk
 
 def create_color_palette(color=None):
     return color
 
+def create_base_css():
+    print("You can either choose a primary color or let us randomize one for you.")
+    primary = ""
+    is_hex = False
+    while not is_hex:
+        primary = input("Enter hex code or press enter for random: ")
+        is_hex = "#" in primary and (len(primary) == 7 or len(primary) == 4)
+        if primary.strip() or not is_hex:
+            print("Your hex code is not a true hex code.")
+        if not primary.strip():
+            break
 
+    if not is_hex:
+        primary = color.generate_random_hex()
+        print(f"The random color we selected for you is {primary}.")
+
+    my_palette = color.color_palette_inator(primary)
+    print(my_palette)
 
 if __name__ == "__main__":
+    create_base_css()
     c1 = create_color_palette("rgb")
-    color2 = create_color_palette()
-    print(c1)
-    print(color2)
-    palette = color.color_palette_inator("#336699")
-    print(palette)
+    # color2 = create_color_palette()
+    # print(c1)
+    # print(color2)
+    # palette = color.color_palette_inator("#336699")
+    # print(palette)
